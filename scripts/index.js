873774a3d5bf5b816a60aa7e2374b421a57428f2
Nikita Kolankov <kolankovnikita@gmail.com>
2024-11-18 18:03:03 +0300

    Инициализация массива 6 карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function addCard() {
+    initialCards.forEach(function (card) {
+        const initialCardElement = cardTemplate.querySelector('.card').cloneNode(true);
+        initialCardElement.querySelector('.card__image').src = card.link;
+        initialCardElement.querySelector('.card__title').textContent = card.name;
+        dltCard(initialCardElement);
+    
+        placesList.append(initialCardElement);
+    });
+}
 
-// @todo: Функция создания карточки
+function dltCard(initialCardElement) {
+    initialCardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+        initialCardElement.remove();
+    });
+}
 
-// @todo: Функция удаления карточки
+addCard();
 
-// @todo: Вывести карточки на страницу
+
+
+
+
+
+
+
